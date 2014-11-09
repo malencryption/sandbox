41d3b4f03be964b644971ff80012168e504d6b82
sunshinedesign36 <runn3rgirl36@gmail.com>
2014-11-09 00:14:57 -0700

    transforms

---
+++ js/transforms.js
@@ -0,0 +1,12 @@
+function one(piece){
+	piece.transform = "rotate(-2deg)"
+}
+function two(piece){
+	piece.transform = "translate(20px, 100px)"
+}
+function three(piece){
+	piece.transform = "scale(2,4)"
+}
+function four(piece){
+	piece.transform = "skew(30deg,20deg)"
+}
